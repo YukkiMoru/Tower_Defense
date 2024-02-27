5817ae5e6b24bb727991735a136cd26e30961563
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-02-27 21:17:27 +0900

    Initial commit

---
+++ src/main/java/org/moru/tower_defense/mob_motion.java
@@ -0,0 +1,15 @@
+package org.moru.tower_defense;
+//ここでは主にmobの動きに関する処理を行います。
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mob_motion {
+
+}
