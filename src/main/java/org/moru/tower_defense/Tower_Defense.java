95de4444b0b32c4a963d9a64f9866a778cbdf880
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-02-27 21:17:27 +0900

    Initial commit

---
+++ src/main/java/org/moru/tower_defense/Tower_Defense.java
@@ -0,0 +1,24 @@
+package org.moru.tower_defense;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tower_Defense extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        // チャットにメッセージを送信("Tower_Defenseプラグインが有効化されました")
+        getServer().getConsoleSender().sendMessage("Tower_Defenseプラグインが有効化されました");
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        // チャットにメッセージを送信("Tower_Defenseプラグインが無効化されました")
+        getServer().getConsoleSender().sendMessage("Tower_Defenseプラグインが無効化されました");
+
+
+    }
+}
