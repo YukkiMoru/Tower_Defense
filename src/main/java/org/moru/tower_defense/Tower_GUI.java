42a5c6c62e1e5723523824a913ecbd98940395ae
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-12 16:03:20 +0900

    Fix: Tower_GUI.java

--- src/main/java/org/moru/tower_defense/Tower_GUI.java
+++ src/main/java/org/moru/tower_defense/Tower_GUI.java
@@ -3,9 +3,12 @@ package org.moru.tower_defense;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
 
 public class Tower_GUI {
 
@@ -16,7 +19,7 @@ public class Tower_GUI {
         // Create a new item stack of dirt
         ItemStack item = new ItemStack(Material.DIRT, 1);
 
-        // Get the item's meta data
+        // Get the item's metadata
         ItemMeta meta = item.getItemMeta();
 
         // Set the item's name
@@ -30,5 +33,11 @@ public class Tower_GUI {
 
         // Open the GUI for the player
         player.openInventory(gui);
+    }
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals("Tower Defense GUI")) {
+            event.setCancelled(true);
+        }
     }
 }
