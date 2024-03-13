8df96471cb672f06a58631faea4a097e9a73dc25
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-14 07:25:29 +0900

    add: コマンドの予測

--- src/main/java/org/moru/tower_defense/TD_Command.java
+++ src/main/java/org/moru/tower_defense/TD_Command.java
@@ -4,10 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class TD_Command implements CommandExecutor {
+public class TD_Command implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
@@ -15,9 +18,6 @@ public class TD_Command implements CommandExecutor {
             if (args.length > 0) {
                 // /td <command> → Execute_Commandが実行される
                 ExecuteCommand(args, "kill", "kill @e[type=!player]");
-
-            } else {
-                player.sendMessage("Usage: /td <kill>");
             }
             return true;
         }
@@ -27,6 +27,20 @@ public class TD_Command implements CommandExecutor {
     private Void ExecuteCommand(String[] args, String command, String Execute_Command) {
         if (args[0].equals(command)) {
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Execute_Command);
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (sender instanceof Player) {
+            if (args.length == 1) {
+                List<String> list = new ArrayList<>();
+                list.add("show");
+                list.add("debug");
+                list.add("kill");
+                return list;
+            }
         }
         return null;
     }
