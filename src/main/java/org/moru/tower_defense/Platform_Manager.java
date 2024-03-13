e9ad5a287b8fb0f2271059ab14ab8ee70e8dd539
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-13 18:22:55 +0900

    WIP: プラットフォームの角の座標を追加

--- src/main/java/org/moru/tower_defense/Platform_Manager.java
+++ src/main/java/org/moru/tower_defense/Platform_Manager.java
@@ -8,8 +8,9 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class Platform_Manager {
     public boolean Platform(Location location, int sizeX, int sizeZ, Material material, PlayerInteractEvent event) {
         //クリックされた場所の座標
-        double x = location.getX();
-        double z = location.getZ();
+        int x = (int) location.getX();
+        int y = (int) location.getY();
+        int z = (int) location.getZ();
 
         //東西南北
         int west = 0, east = 0, south = 0, north = 0;
@@ -25,61 +26,53 @@ public class Platform_Manager {
             north--;
 
         //東西南北のブロックの数を数える
-        int distanceX = east - west + 1;
-        int distanceY = south - north + 1;
+        int PlatformX = east - west + 1;
+        int PlatformY = south - north + 1;
 
-        //中心座標
-        double centerX = (east + west) / 2;
-        double centerZ = (south + north) / 2;
+        //Edge座標
+        int EdgeX = x + west;
+        int EdgeZ = z + north;
 
         //デバッグゾーン
-        if (distanceX % 2 == 0) {
-            centerX += 0.5;
-        }
-        if (distanceY % 2 == 0) {
-            centerZ += 0.5;
-        }
         Player player = event.getPlayer();
         player.sendMessage("West: " + -west);
         player.sendMessage("East: " + east);
         player.sendMessage("South: " + south);
         player.sendMessage("North: " + -north);
-        player.sendMessage("Platform_Size_X: " + distanceX);
-        player.sendMessage("Platform_Size_Y: " + distanceY);
-        if (distanceX % 2 == 0) {
-            centerX += 0.5;
-        }
-        if (distanceY % 2 == 0) {
-            centerZ += 0.5;
-        }
-        player.sendMessage("Center " + centerX + " " + (location.getY()) + " " + centerZ);
-        boolean IsPlatformFilled = true;
-        for (int i = 0; i <= north + south; i++) {
-            for (int j = 0; j <= west + east; j++) {
-                if (location.getWorld().getBlockAt(new Location(location.getWorld(), x + j, location.getY(), z + i)).getType() != material) {
-                    IsPlatformFilled = false;
-                }
-            }
-        }
-        if (IsPlatformFilled) {
-            player.sendMessage("You clicked part of " + distanceX + " * " + distanceY + " " + material + "!");
-        } else {
-            player.sendMessage("This Platform is not filled with " + material + "!");
-        }
+        player.sendMessage("Platform_Size_X: " + PlatformX);
+        player.sendMessage("Platform_Size_Y: " + PlatformY);
+        player.sendMessage("EdgeX: " + EdgeX + ", EdgeZ: " + EdgeZ);
+        player.sendMessage("Clicked " + (location.getX()) + " " + (location.getY()) + " " + (location.getZ()));
 
 
-        //プラットフォームの判定
-        if (distanceX == sizeX && distanceY == sizeZ) {
-            for (int i = 0; i <= north + south; i++) {
-                for (int j = 0; j <= west + east; j++) {
-                    if (location.getWorld().getBlockAt(new Location(location.getWorld(), x + j, location.getY(), z + i)).getType() != material) {
-                        return false;
-                    }
-                }
-            }
-        } else {
-            return false;
-        }
-        return true;
+//        player.sendMessage("Center " + centerX + " " + (location.getY()) + " " + centerZ);
+//        boolean IsPlatformFilled = true;
+//        for (int i = 0; i <= north + south; i++) {
+//            for (int j = 0; j <= west + east; j++) {
+//                if (location.getWorld().getBlockAt(new Location(location.getWorld(), x + j, location.getY(), z + i)).getType() != material) {
+//                    IsPlatformFilled = false;
+//                }
+//            }
+//        }
+//        if (IsPlatformFilled) {
+//            player.sendMessage("You clicked part of " + distanceX + " * " + distanceY + " " + material + "!");
+//        } else {
+//            player.sendMessage("This Platform is not filled with " + material + "!");
+//        }
+//
+//
+//        //プラットフォームの判定
+//        if (distanceX == sizeX && distanceY == sizeZ) {
+//            for (int i = 0; i <= north + south; i++) {
+//                for (int j = 0; j <= west + east; j++) {
+//                    if (location.getWorld().getBlockAt(new Location(location.getWorld(), x + j, location.getY(), z + i)).getType() != material) {
+//                        return false;
+//                    }
+//                }
+//            }
+//        } else {
+//            return false;
+//        }
+        return false;
     }
 }
