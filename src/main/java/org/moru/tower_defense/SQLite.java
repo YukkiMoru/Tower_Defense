9223f3260132e52d562a612294478e5ab0f4b369
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-14 07:25:54 +0900

    add: SQLite.java

--- src/main/java/org/moru/tower_defense/SQLite.java
+++ src/main/java/org/moru/tower_defense/SQLite.java
@@ -1,5 +1,27 @@
 package org.moru.tower_defense;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLite {
+    private Connection connection;
+
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:TdSql.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
