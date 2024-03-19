a0e6ffed6817213c56598b36a4324ce99a4aa5af
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-19 23:02:14 +0900

    WIP: wardenの頭を追加しようとした。

--- src/main/java/org/moru/tower_defense/InventoryGUI.java
+++ src/main/java/org/moru/tower_defense/InventoryGUI.java
@@ -5,13 +5,27 @@ import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.UUID;
 
 public class InventoryGUI {
 
     public static Inventory TowerGUI() {
         Inventory gui_test = createInventory("TowerGUI", 27);
         ItemStack diamonds = createItem(Material.DIAMOND, 1, "Click me!");
+
+        // Create the player head item
+        ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
+        skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(UUID.fromString("1531857479-78233159-1681038604-1859460671")));
+        skullMeta.setDisplayName("Warden");
+        playerHead.setItemMeta(skullMeta);
+
+        // Add the items to the inventory
         gui_test.setItem(0, diamonds);
+        gui_test.setItem(1, playerHead);  // Add the player head to the inventory
+
         return gui_test;
     }
 
