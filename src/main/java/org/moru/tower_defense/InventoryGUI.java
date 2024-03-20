b869855da6ddff8bffe8927f68796d5f3eb9c147
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-20 16:21:25 +0900

    add: player headの追加

--- src/main/java/org/moru/tower_defense/InventoryGUI.java
+++ src/main/java/org/moru/tower_defense/InventoryGUI.java
@@ -1,5 +1,7 @@
 package org.moru.tower_defense;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
@@ -7,18 +9,30 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 public class InventoryGUI {
-
     public static Inventory TowerGUI() {
         Inventory gui_test = createInventory("TowerGUI", 27);
+
         ItemStack diamonds = createItem(Material.DIAMOND, 1, "Click me!");
 
         // Create the player head item
         ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
-        skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(UUID.fromString("1531857479-78233159-1681038604-1859460671")));
+
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "Warden");
+        profile.getProperties().put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmNmMzY3NGIyZGRjMGVmN2MzOWUzYjljNmI1ODY3N2RlNWNmMzc3ZDJlYjA3M2YyZjNmZTUwOTE5YjFjYTRjOSJ9fX0="));
+
+        try {
+            Field profileField = skullMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(skullMeta, profile);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         skullMeta.setDisplayName("Warden");
         playerHead.setItemMeta(skullMeta);
 
