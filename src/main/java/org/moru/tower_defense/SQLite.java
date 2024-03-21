fe6a8abb7434d495da6417118251444f9b7e2d07
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-21 14:32:06 +0900

    add:  createTableIfNotExists関数 sqlのテーブルがない場合作成する。

--- src/main/java/org/moru/tower_defense/SQLite.java
+++ src/main/java/org/moru/tower_defense/SQLite.java
@@ -2,10 +2,35 @@ package org.moru.tower_defense;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SQLite {
     private Connection connection;
+
+    public void createTableIfNotExists() {
+        connect();
+        Connection conn = getConnection();
+        PreparedStatement pstmt = null;
+
+        String sql = "CREATE TABLE IF NOT EXISTS tower_data(TowerID INTEGER, TowerName TEXT, TowerType INTEGER, level INTEGER)";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            disconnect();
+        }
+    }
 
     public void connect() {
         try {
