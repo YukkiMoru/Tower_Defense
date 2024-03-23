7c0428718cd0f955f7abf2b82714d0f63b15c320
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-23 20:38:36 +0900

    fix: コメントアウトの修正

--- src/main/java/org/moru/tower_defense/Tower_Defense.java
+++ src/main/java/org/moru/tower_defense/Tower_Defense.java
@@ -9,33 +9,30 @@ public final class Tower_Defense extends JavaPlugin {
     private SQLite sqlite = new SQLite();
     @Override
     public void onEnable() {
-        // mob_motionの起動
+        // MobMotionの起動
         getServer().getPluginManager().registerEvents(new ListenerMob(this), this);
 
-        // Buildingの起動
-        ListenerBlock buildingInstance = new ListenerBlock();
-        getServer().getPluginManager().registerEvents(buildingInstance, this);
+        // ListenerBlockの起動
+        getServer().getPluginManager().registerEvents(new ListenerBlock(), this);
 
-        // TD_Commandの起動
+        // CommandTDの起動
         getCommand("td").setExecutor(new CommandTD());
 
         // SQLiteManagerTowerの起動
         sqliteManagerTower = SQLiteManagerTower.getInstance();
 
-        //InventoryClickListenerの起動
+        //ListenerInventoryClickの起動
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new ListenerInventoryClick(), this);
 
-        // チャットにメッセージを送信("Tower_Defenseプラグインが有効化されました")
         getServer().getConsoleSender().sendMessage("Tower_Defenseプラグインが有効化されました");
     }
 
     @Override
     public void onDisable() {
-        // shutdown sqlite
+        // SQLiteのシャットダウン
         sqlite.shutdown();
 
-        // チャットにメッセージを送信("Tower_Defenseプラグインが無効化されました")
         getServer().getConsoleSender().sendMessage("Tower_Defenseプラグインが無効化されました");
     }
 }
