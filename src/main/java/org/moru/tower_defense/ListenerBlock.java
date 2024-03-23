21df45dea8c43473a9350d0ab63037cb0581f3da
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-23 20:36:56 +0900

    update: 関数化、PlatformClickとTowerInteractの実装

--- src/main/java/org/moru/tower_defense/ListenerBlock.java
+++ src/main/java/org/moru/tower_defense/ListenerBlock.java
@@ -13,32 +13,37 @@ import org.bukkit.inventory.Inventory;
 public class ListenerBlock implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        ManagerPlatform managerPlatform = ManagerPlatform.getInstance();
-
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getClickedBlock().getType() == Material.CHERRY_PLANKS) {
-                if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR) {
-                    Location Edgelocation = managerPlatform.Platform(event.getClickedBlock().getLocation(), 3, 3, Material.CHERRY_PLANKS, event);
-                    if (Edgelocation != null) {
-                        Player player = event.getPlayer();
-                        Inventory gui = InventoryGUI.TowerGUI();
-                        player.openInventory(gui);
-                    }
+            // プラットフォームかどうかを判定
+            PlatformClick(event.getClickedBlock().getType(), event);
+
+            // タワーかどうかを判定
+            TowerInteract(event);
+        }
+    }
+
+    private void PlatformClick(Material clickedBlock, PlayerInteractEvent event) {
+        ManagerPlatform managerPlatform = ManagerPlatform.getInstance();
+        if (clickedBlock == Material.CHERRY_PLANKS) {
+            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR) {
+                Location edgeLocation = managerPlatform.Platform(event.getClickedBlock().getLocation(), 3, 3, Material.CHERRY_PLANKS, event);
+                if (edgeLocation != null) {
+                    Player player = event.getPlayer();
+                    Inventory gui = InventoryGUI.TowerGUI();
+                    player.openInventory(gui);
                 }
             }
+        }
+    }
 
-            // Get the clicked block's coordinates
-            Location clickedBlockLocation = event.getClickedBlock().getLocation();
-
-            // get an instance of SQLiteManagerTower
-            SQLiteManagerTower sqliteManagerTower = SQLiteManagerTower.getInstance();
-
-            // Get the TowerID from the SQLite database
-            int TowerID = sqliteManagerTower.GetTowerID(clickedBlockLocation);
-
+    private void TowerInteract(PlayerInteractEvent event) {
+        // Get the clicked block's coordinates
+        Location clickedBlockLocation = event.getClickedBlock().getLocation();
+        SQLiteManagerTower sqliteManagerTower = SQLiteManagerTower.getInstance();
+        int TowerID = sqliteManagerTower.GetTowerID(clickedBlockLocation);
+        if (TowerID != 0) {
             Player player = (Player) event.getPlayer();
             player.sendMessage("TowerID " + TowerID + " がクリックされました!");
-
         }
     }
 }
