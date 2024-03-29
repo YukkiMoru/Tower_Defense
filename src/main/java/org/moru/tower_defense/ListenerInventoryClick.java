aacdc653865c8873646315afd34329ecc9708ea0
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-03-29 17:06:40 +0900

    add: SyncGUI

--- src/main/java/org/moru/tower_defense/ListenerInventoryClick.java
+++ src/main/java/org/moru/tower_defense/ListenerInventoryClick.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -109,8 +110,9 @@ public class ListenerInventoryClick implements Listener {
                         int ClickedTowerID = listenerBlock.getCurrentTowerID();
 
                         towerManager.UpgradeTower(ClickedTowerID);
-                        //reload gui
-                        InventoryGUI.TowerGUI(ClickedTowerID);
+                        //変更後のGUIにリロード
+                        Inventory newGui = InventoryGUI.TowerGUI(ClickedTowerID);
+                        player.openInventory(newGui);
                         player.playSound(player.getLocation(), "minecraft:block.anvil.destroy", 1.0f, 0.5f);
                         //タワーをアップグレード
                         SQLiteManagerTower sqliteManagerTower = SQLiteManagerTower.getInstance();
