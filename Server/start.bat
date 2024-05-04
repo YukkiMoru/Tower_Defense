1d5f26f65fe8ddea1005cf9ffe1dcedee12bfcd5
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-05-04 21:26:33 +0900

    update: java-21

--- Server/start.bat
+++ Server/start.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xms8G -Xmx8G -jar paper-1.20.4-459.jar nogui
+"C:\Program Files\Java\jdk-21\bin\java.exe" -Xms8G -Xmx8G -jar paper-1.20.4-459.jar nogui
 exit
